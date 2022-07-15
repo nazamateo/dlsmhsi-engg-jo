2605cf9869866d911997ecc07fe5feb2d6660982
nazamateo <nazamateo@gmail.com>
2022-07-15 11:16:09 +0800

    Generate scaffold for admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,14 @@ class HomeController < ApplicationController
     before_action :authenticate_user!
     def index
     end
+
+    private
+    
+    def authenticate_admin
+    end
+
+    def authenticate_requestor
+    end
+
+
 end
