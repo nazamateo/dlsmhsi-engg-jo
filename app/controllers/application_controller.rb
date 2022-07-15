88596c88feadf7c4e80d06292c519b41735f99d7
nazamateo <nazamateo@gmail.com>
2022-07-15 10:44:50 +0800

    Set home page authentication
    Redirects to sign in page if user is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    protected
+    def authenticate_user!
+        if user_signed_in?
+            super
+        else
+        redirect_to new_user_session_path, :notice => 'Please sign in to your account'
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 end
