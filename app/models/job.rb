890d29a1e1fc025c78d0e0e2839e27f7e950b7b9
nazamateo <nazamateo@gmail.com>
2022-07-15 13:54:04 +0800

    Add validations for Jobs mode

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,8 @@ class Job < ApplicationRecord
     belongs_to :assessor, class_name: 'Personnel', foreign_key: 'assessor', required: true
     belongs_to :staff, class_name: 'Personnel', foreign_key: 'staff', required: true
 
-    validates: 
+    validates :description, length: { in: 10..80 }
+    validates :maintenance_unit, inclusion: { in: ['Admin', 'Aircon', 'Electrical', 'Plumbing', 'Masonry', 'Carpentry', 'Welding', 'Painting', 'Outside']}
+    validates :status, inclusion: { in: ['Pending', 'Ongoing', 'Onhold', 'Done', 'Cancel']}
+    validates :remarks, length: { maximum: 20 }
 end
--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -2,9 +2,7 @@ class Personnel < ApplicationRecord
     has_many :jobs, dependent: :destroy
 
     validates :name, :surname, presence: true
-    validates :maintenance_unit, inclusion: { in: ['Admin', 'Aircon', 'Electrical', 'Plumbing', 'Masonry', 'Carpentry', 'Welding', 'Painting', 'Outside']}
+    validates :maintenance_unit, inclusion: { in: ['Admin', 'Aircon', 'Electrical', 'Plumbing', 'Masonry', 'Carpentry', 'Welding', 'Painting']}
     validates :position, inclusion: { in: ['Engineer', 'Aircon-Tech', 'Electrician', 'Plumber', 'Mason', 'Carpenter', 'Welder', 'Painter']}
-    t.string :position
-
 
 end
