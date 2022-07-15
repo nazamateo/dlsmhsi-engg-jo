cd6067ce92cba5a62710054b794b7340942cf176
nazamateo <nazamateo@gmail.com>
2022-07-15 13:45:45 +0800

    Add validations for Personnel model

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -1,3 +1,10 @@
 class Personnel < ApplicationRecord
     has_many :jobs, dependent: :destroy
+
+    validates :name, :surname, presence: true
+    validates :maintenance_unit, inclusion: { in: ['Admin', 'Aircon', 'Electrical', 'Plumbing', 'Masonry', 'Carpentry', 'Welding', 'Painting', 'Outside']}
+    validates :position, inclusion: { in: ['Engineer', 'Aircon-Tech', 'Electrician', 'Plumber', 'Mason', 'Carpenter', 'Welder', 'Painter']}
+    t.string :position
+
+
 end
